69326e4cf2f9b5074a33ef00d091530165b9d5f7
Ivan Proios <ivan.proios@sda.se>
2025-08-15 15:56:18 +0200

    WIP: keycloak

--- iban-blacklist-service/src/main/java/chlng/e1proios/BlacklistController.java
+++ iban-blacklist-service/src/main/java/chlng/e1proios/BlacklistController.java
@@ -1,13 +1,16 @@
 package chlng.e1proios;
 
+import io.quarkus.security.Authenticated;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.jboss.resteasy.reactive.NoCache;
 import org.jboss.resteasy.reactive.RestResponse;
 
 @Path("/api/blacklist")
+@Authenticated
 public class BlacklistController {
 
     @Inject
@@ -15,6 +18,7 @@ public class BlacklistController {
 
     @GET
     @Path("/")
+    @NoCache
     @Produces(MediaType.APPLICATION_JSON)
     public RestResponse<String[]> getBlacklistedIbans() {
         System.out.println("GETTING BLACKLISTED IBANS");
