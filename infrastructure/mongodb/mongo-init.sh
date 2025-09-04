6ba41278224fc500c7805fe8c193396d7fd99317
Ivan Proios <ivan.proios@sda.se>
2025-09-04 13:36:26 +0200

    replace hard-coded constants with env var, this time without breaking functionality

--- infrastructure/mongodb/mongo-init.sh
+++ infrastructure/mongodb/mongo-init.sh
@@ -1,12 +1,16 @@
+set -e
+mongosh <<EOF
+use admin;
 db = db.getSiblingDB('blacklist');
 db.createCollection('ibans');
 db.createUser({
-  user: "user",
-  pwd: "password",
-  roles: [{ role: "read", db: "blacklist" }]
+  user: '$MONGO_DB_USER_LOGIN',
+  pwd:  '$MONGO_DB_USER_PASSWORD',
+  roles: [{
+    role: 'readWrite',
+    db: '$MONGO_DB'
+  }]
 });
-
-// Create collections and insert data
 db.ibans.insertMany(
   [
     "FR14 2004 1010 0505 0001 3M02 606",
@@ -17,3 +21,4 @@ db.ibans.insertMany(
     "DE15 3006 0601 0505 7807 80"
   ].map(givenIban => ({ iban: givenIban }))
 );
+EOF
