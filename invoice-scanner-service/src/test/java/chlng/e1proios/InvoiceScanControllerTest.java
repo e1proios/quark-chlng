e4e45ce1e26f6a52219914fe88a4e5905f5140df
Ivan Proios <ivan.proios@sda.se>
2025-09-08 22:55:17 +0200

    implement unit tests for the invoice scanner controller

--- invoice-scanner-service/src/test/java/chlng/e1proios/InvoiceScanControllerTest.java
+++ invoice-scanner-service/src/test/java/chlng/e1proios/InvoiceScanControllerTest.java
@@ -4,12 +4,11 @@ import io.quarkus.test.InjectMock;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.security.TestSecurity;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-
-import static org.mockito.ArgumentMatchers.any;
 
 @QuarkusTest
 public class InvoiceScanControllerTest {
@@ -29,15 +28,59 @@ public class InvoiceScanControllerTest {
     }
 
     @Test
-    public void testScanEndpoint() {
-        try {
-            Mockito.when(
-                scanService.checkPdfForBlacklistedIbans(any(String.class))
-            ).thenReturn(
-                new InvoiceScanService.PdfInfo()
-            );
-        } catch (Exception e) {
-            // do nothing
-        }
+    @TestSecurity(authorizationEnabled = false)
+    public void testScanEndpointEmptyUrl() {
+        var scanRequestPayload = new InvoiceScanController.ScanRequestPayload("");
+
+        RestAssured.given()
+            .contentType(ContentType.JSON)
+            .body(scanRequestPayload)
+            .when()
+            .post("/api/scan")
+            .then()
+            .statusCode(404);
+    }
+
+    @Test
+    @TestSecurity(authorizationEnabled = false)
+    public void testScanEndpointError() throws Exception {
+        var scanRequestPayload = new InvoiceScanController.ScanRequestPayload("ERROR_URL");
+
+        Mockito.when(
+            scanService.checkPdfForBlacklistedIbans("ERROR_URL")
+        ).thenThrow(
+            new RuntimeException("simulated exception message")
+        );
+
+        RestAssured.given()
+            .contentType(ContentType.JSON)
+            .body(scanRequestPayload)
+            .when()
+            .post("api/scan")
+            .then()
+            .statusCode(500);
+    }
+
+    @Test
+    @TestSecurity(authorizationEnabled = false)
+    public void testScanEndpointSuccess() throws Exception {
+        var scanRequestPayload = new InvoiceScanController.ScanRequestPayload("SUCCESS_URL");
+
+        Mockito.when(
+            scanService.checkPdfForBlacklistedIbans("SUCCESS_URL")
+        ).thenReturn(
+            new InvoiceScanService.PdfInfo("success_url", false, "", -1)
+        );
+
+        Response scanSuccess = RestAssured.given()
+            .contentType(ContentType.JSON)
+            .body(scanRequestPayload)
+            .when()
+            .post("api/scan");
+
+        Assertions.assertEquals(200, scanSuccess.getStatusCode());
+
+        var scanSuccessBody = scanSuccess.as(InvoiceScanService.PdfInfo.class);
+        Assertions.assertEquals("success_url", scanSuccessBody.srcUrl());
     }
 }
